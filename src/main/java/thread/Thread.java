f23770c17e0b7cd9b461f6e2ee3b1617cd0106f0
汪翔 <xiang.wang@zknow.com>
2022-08-29 11:33:26 +0800

    [IMP] 夏日寒风

---
+++ src/main/java/thread/Thread.java
@@ -0,0 +1,19 @@
+package thread;
+
+import java.util.Date;
+
+public class Thread {
+
+    public static void main(String[] args) {
+        new java.lang.Thread(() -> {
+            while (true){
+                try {
+                    java.lang.Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(new Date().toString());
+            }
+        }).start();
+    }
+}
