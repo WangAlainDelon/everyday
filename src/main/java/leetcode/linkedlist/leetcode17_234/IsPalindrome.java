379bd40c4f1b9e7acadda40379d6be7b4b2ee0f7
汪翔 <xiang.wang@zknow.com>
2022-09-21 19:59:09 +0800

    [IMP]临门一脚

---
+++ src/main/java/leetcode/linkedlist/leetcode17_234/IsPalindrome.java
@@ -0,0 +1,21 @@
+package leetcode.linkedlist.leetcode17_234;
+
+import leetcode.linkedlist.common.ListNode;
+
+public class IsPalindrome {
+
+    // 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+    // 回文就是正的跟反的读都是一样的
+    // 如果是字符或者数组都好办，可以利用指针碰撞的方式来判断是不是回文
+    public static void main(String[] args) {
+
+    }
+
+
+    // 思路：快慢指针的方式
+    // 1.中分链表  反转一半  然后再两个指针判断
+    public boolean isPalindrome(ListNode head) {
+
+        return true;
+    }
+}
