8108a0a4c8270a1d4ecdbdae76f7b3b3e75f21f8
汪翔 <xiang.wang@zknow.com>
2022-12-12 10:30:37 +0800

    A

---
+++ src/main/java/base/reflec/ReflectTest.java
@@ -0,0 +1,11 @@
+package base.reflec;
+
+public class ReflectTest {
+
+    public static void main(String[] args) {
+
+        String ref = "base.reflec.UserServiceImpl.createUser";
+
+
+    }
+}
---
+++ src/main/java/base/reflec/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package base.reflec;
+
+public class UserServiceImpl {
+
+
+    public void createUser() {
+
+        System.out.println("创建用户成功");
+    }
+
+}
